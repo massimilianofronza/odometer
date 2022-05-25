d7fe93bb36d6620e9e055c5d5886702ee9197178
massimilianofronza <maxfronza97@gmail.com>
2022-05-25 16:00:51 +0200

    Merging Documentation functions with my solution

--- odometer_testing.m
+++ odometer_testing.m
@@ -5,47 +5,54 @@
 %       May 2022
 
 close all;
-%clear all;
+clear all; % Slows down computation since it de-allocates the variables
 clc;
 
+% Read the image
 % img = imread('odometro1.jpg');
 img = imread('test.jpg');
-% img = imread('radiant.jpg');
+% img = imread('radiant.jpg');  % For detecting a range of angles
 
-% Hard-coded coordinates of rectangle
-rect = [545 594 335 145];
+% (1a) Hard-coded coordinates of rectangle
+% rect = [545 594 335 145];
+rect = [192 184 180 188];
 
-% Select the Region of Interest(ROI) by hand
-figure; imshow(img); title('Odometro1.jpg');    % Comment this...
-rect = getrect;         % ...and this if you want the fixed coordinates
+% (1b) Select the Region of Interest(ROI) by hand
+% figure; imshow(img); title('Odometro1.jpg');    % Comment this...
+% rect = getrect;         % ...and this if you want the fixed coordinates
+
+% (2) Crop the ROI
 ROI = imcrop(img, rect);
-close all;
 
 %figure; imshow(ROI); title('ROI');
 %figure; imhist(ROI); title('ROI - Histogram');
 
-% Tests with gray scale 1
+% Tests with gray scale
 %grayImage = ind2gray(ROI, gray(200));
 %figure; imshow(grayImage); title('ROI gray');
 %figure; imhist(grayImage); title('ROI gray - Histogram');
 
-% Test with gray scale 2
+% (3a) Convert into gray scale the 3-dimensional image
 grayROI = rgb2gray(ROI);
-figure; imshow(grayROI); title('My attempt');%title('ROI gray');
+% figure; imshow(grayROI); title('My attempt');%title('ROI gray');
 
-% If you use the radiant image, uncheck this
+% (3b) If you use the radiant image
 % grayROI = ROI;
 
-% Extract the edges of the gray image
-edges = edge(grayROI, "canny");
+% (4) Extract the edges of the gray image
+edges_canny = edge(grayROI, "canny");
 
-% Horizontal angles to be identified in our case
+% (5) Horizontal angles to be identified in our case
 angles = [-90:1:-45, 45:1:89];
 
-[H, theta, rho] = hough(edges, 'RhoResolution', 1, 'Theta', angles); %'Theta', -90:0.5:89);
+% (6) Run te Hough Lines algorithm for the detection of horizontal lines
+[H, theta, rho] = hough(edges_canny, 'RhoResolution', 1, 'Theta', angles); %'Theta', -90:0.5:89);
 
-% Get rows and thetas of lines occurring above the {threshold}
-threshold = 70;
+% (7a) Identify a set of peaks in the Hough accumulation matrix
+P = houghpeaks(H, 5);
+
+% (7b) Get rows and thetas of lines occurring above the {threshold}
+threshold = 115;
 logic_nonzero = H>=threshold;
 [rows, cols] = find(logic_nonzero);
 
@@ -53,15 +60,39 @@ logic_nonzero = H>=threshold;
 % [rows, cols] = find(ismember(H, max(H(:))));
 % [rows, cols] = find(ismember(H, max(max(H))));   % alternative method
 
-% Plot every line above the {threshold}
+% (8) Find the top P lines previously identified
+% TODO Fillgap and MinLength are the ones I have to use to obtain continuous lines
+lines = houghlines(edges_canny, theta, rho, P, 'FillGap', 5, 'MinLength', 7);
+
+close all;
+
+% (9a) Plot every peak line - Documentation example
+figure, imshow(grayROI), title('Detected lines on ROI'), hold on;
+max_len = 0;
+for k = 1:length(lines)
+    xy = [lines(k).point1; lines(k).point2];
+    plot(xy(:,1),xy(:,2), 'LineWidth', 2, 'Color', 'green');
+
+    % Determine the endpoints of the longest line segment
+    len = norm(lines(k).point1 - lines(k).point2);
+    if ( len > max_len)
+        max_len = len;
+        xy_long = xy;
+    end
+end
+% highlight the longest line segment
+plot(xy_long(:,1),xy_long(:,2),'LineWidth',2,'Color','red');
+hold off;
+
+% (9b) Plot every line above the {threshold} - my method
+figure, imshow(grayROI), title('My detected lines on ROI'), hold on;
 x = 0:size(grayROI, 2);
 y = 0:size(grayROI, 2);
-hold on;
-for i = 1:size(rows, 1)
+for i = 1:size(rows, 1) % change to length(rows)
     for j = 1:size(x, 2)
         y(j) = (rho(rows(i))-x(j)*cos(theta(cols(i))))/sin(theta(cols(i)));
     end
-%     y = -y; % We need this to be able to plot on the actual figure or maybe
+    y = -y; % We need this to be able to plot on the actual figure or maybe
             % not because the points were already computed on the gray 
             % image. Who knows
     plot(x, y, 'LineWidth',2,'Color','yellow'); %'-oy');
@@ -84,9 +115,10 @@ hold off;
 % end
 % hold off;
 
-disp('all done');
+disp('all done.');
+disp('Printing rows and cols actual rhos and thetas:');
 
-for i = 1:size(rows, 1)
+for i = 1:length(rows)
     log = sprintf('%d, %d', rho(rows(i)), theta(cols(i)));
     disp(log);
 end
@@ -104,27 +136,27 @@ end
 
 
 % MATLAB DOCUMENTATION EXAMPLES:
-BW = edge(grayROI,'canny');
-
-[H,theta,rho] = hough(BW, 'RhoResolution', 1, 'Theta', angles);
-
-P = houghpeaks(H,14,'threshold',ceil(0.3*max(H(:))));
-
-lines = houghlines(BW,theta,rho,P,'FillGap',5,'MinLength',7);
-
-figure, imshow(grayROI), title('Matlab documentation example'), hold on
-max_len = 0;
-for k = 1:length(lines)
-   xy = [lines(k).point1; lines(k).point2];
-   plot(xy(:,1),xy(:,2),'LineWidth',2,'Color','green');
-
-   % Determine the endpoints of the longest line segment
-   len = norm(lines(k).point1 - lines(k).point2);
-   if ( len > max_len)
-      max_len = len;
-      xy_long = xy;
-   end
-end
-% highlight the longest line segment
-plot(xy_long(:,1),xy_long(:,2),'LineWidth',2,'Color','red');
+% BW = edge(grayROI,'canny');
+% 
+% [H,theta,rho] = hough(BW, 'RhoResolution', 1, 'Theta', angles);
+% 
+% P = houghpeaks(H,14,'threshold',ceil(0.3*max(H(:))));
+% 
+% lines = houghlines(BW,theta,rho,P,'FillGap',5,'MinLength',7);
+% 
+% figure, imshow(grayROI), title('Matlab documentation example'), hold on
+% max_len = 0;
+% for k = 1:length(lines)
+%    xy = [lines(k).point1; lines(k).point2];
+%    plot(xy(:,1),xy(:,2),'LineWidth',2,'Color','green');
+% 
+%    % Determine the endpoints of the longest line segment
+%    len = norm(lines(k).point1 - lines(k).point2);
+%    if ( len > max_len)
+%       max_len = len;
+%       xy_long = xy;
+%    end
+% end
+% % highlight the longest line segment
+% plot(xy_long(:,1),xy_long(:,2),'LineWidth',2,'Color','red');
 
