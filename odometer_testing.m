87492cb963fca205dfa99a8fe88f25c61e09020c
massimilianofronza <maxfronza97@gmail.com>
2022-06-14 00:36:35 +0200

    Achieve identification of horizontal lines and minor fixes

--- odometer_testing.m
+++ odometer_testing.m
@@ -9,13 +9,13 @@ clear all; % Slows down computation since it de-allocates the variables
 clc;
 
 % Read the image
-% img = imread('odometro1.jpg');
-img = imread('test.jpg');
+img = imread('odometro1.jpg');
+% img = imread('test.jpg');
 % img = imread('radiant.jpg');  % For detecting a range of angles
 
 % (1a) Hard-coded coordinates of rectangle
-% rect = [545 594 335 145];
-rect = [192 184 180 188];
+rect = [545 594 335 145];
+% rect = [192 184 180 188];
 
 % (1b) Select the Region of Interest(ROI) by hand
 % figure; imshow(img); title('Odometro1.jpg');    % Comment this...
@@ -41,10 +41,10 @@ angles = [-90:1:-45, 45:1:89];
 [H, theta, rho] = hough(edges_canny, 'RhoResolution', 1, 'Theta', angles); %'Theta', -90:0.5:89);
 
 % (7a) Identify a set of peaks in the Hough accumulation matrix
-doc_peaks = houghpeaks(H, 5);
+doc_peaks = houghpeaks(H, 50);
 
 % (7b) Get rows and thetas of lines occurring above the {threshold}
-threshold = 135;
+threshold = 100;
 logic_nonzero = H>=threshold;
 [rows, cols] = find(logic_nonzero);
 my_peaks = [rows, cols];
@@ -52,17 +52,48 @@ my_peaks = [rows, cols];
 % TODO FINO A QUI TUTTO BENE IN TEORIA, VERIFICA I VALORI DI peaks ANCHE
 % CON QUELLI SUL FOGLIO.
 
-% (8a) Find the top P lines previously identified
-% TODO Fillgap and MinLength are the ones I have to use to obtain continuous lines
-doc_lines = houghlines(edges_canny, theta, rho, doc_peaks, 'FillGap', 20, 'MinLength', 7);
-my_lines = houghlines(edges_canny, theta, rho, my_peaks, 'FillGap', 20, 'MinLength', 7);
-lines = doc_lines;       % Change this to see which version is in use
+% (8) Find the top P lines previously identified
+% Fillgap: Distance between two line segments associated with the same
+% Hough transform bin - default: 20
+% MinLength: Minimum line length - default: 40
+doc_lines = houghlines(edges_canny, theta, rho, doc_peaks, 'FillGap', 50, 'MinLength', 50);
+my_lines = houghlines(edges_canny, theta, rho, my_peaks, 'FillGap', 50, 'MinLength', 50);
 
 % TODO: note on the .odt document about houghpeaks and my method.
 close all;
 
-% (9a) Plot every peak line - Documentation example
-figure, imshow(grayROI), title('Detected lines on ROI'), hold on;
+% (9a) Plot every peak line - Documentation lines
+lines = doc_lines;
+figure, imshow(grayROI), title('Detected lines on ROI Documentation'), hold on;
+max_len = 0;        % Length of the longest line
+xy_long = 0;        % xy coordinates of the longest line
+for i = 1:length(lines)
+    xy = [lines(i).point1; lines(i).point2];
+    plot(xy(:, 1), xy(:, 2), 'LineWidth', 2, 'Color', 'green');
+
+    % Determine the length of the line.
+    % norm(v) returns the Euclidean norm given the vector v
+    len = norm(lines(i).point1 - lines(i).point2);
+    if (len > max_len)
+        max_len = len;
+        xy_long = xy;
+    end
+
+    % Progression output
+    log = sprintf('%d/%d', i, size(lines, 2));
+    disp(log);
+end
+% highlight the longest line segment
+if xy_long ~= 0
+    plot(xy_long(:,1), xy_long(:,2), 'LineWidth', 2, 'Color', 'red');
+else
+    error("MY DEBUG INFO: No lines were found!");
+end
+hold off;
+
+% (9b) Plot every peak line - My lines
+lines = my_lines;
+figure, imshow(grayROI), title('Detected lines on ROI my lines'), hold on;
 max_len = 0;
 for i = 1:length(lines)
     xy = [lines(i).point1; lines(i).point2];
@@ -85,24 +116,23 @@ plot(xy_long(:,1), xy_long(:,2), 'LineWidth', 2, 'Color', 'red');
 hold off;
 
 disp('all done.');
-disp('Printing rows and cols actual rhos and thetas:');
 
+% disp('Printing rows and cols actual rhos and thetas:');
 % for i = 1:length(rows)
 %     log = sprintf('%d, %d', rho(rows(i)), theta(cols(i)));
 %     disp(log);
 % end
 
-disp(' DEBUGGING NOW')
-
-% Debug code:
-peaks = doc_peaks
-for i = 1:size(peaks, 1)
-%     disp(peaks(i, :))
-%     disp(H(peaks(i, 1), peaks(i, 2)))
+disp(' DEBUGGING NOW');
+peaks = doc_peaks;
+disp("Printing doc peaks");
+for j = 1:2
+    disp(peaks);
+    for i = 1:size(peaks, 1)
+        disp(H(peaks(i, 1), peaks(i, 2)));
+    end
+    if j == 1
+        disp("Printing my peaks");
+        peaks = my_peaks;
+    end
 end
-
-% for i = 1:size(peaks, 1)
-%     log = sprintf('%d - %d', rows(i), cols(i));
-%     disp(log)
-%     disp(H(rows(i), cols(i)))
-% end
