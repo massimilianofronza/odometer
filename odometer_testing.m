2771f023d4d619d90264050e2be5e4da0c2580d7
massimilianofronza <maxfronza97@gmail.com>
2022-06-29 00:37:18 +0200

    Merge the plotting code blocks for a shorter script

--- odometer_testing.m
+++ odometer_testing.m
@@ -12,7 +12,7 @@ clc;
 %%% Global settings
 IMAGES = "./odometers/";    % Images folder 
 DEBUG = false;              % If true, shows debug info in the console
-FILE = 1;                   % File number to pick from the images folder
+FILE = 3;                   % File number to pick from the images folder
 FIXED_ROI = false;          % If true, picks the hard-coded ROI. If false, take it manually
 N_PEAKS = 10;               % Amount of desired peaks in the first identification method
 HOUGH_THRESHOLD = 105;      % The more confused the image, the higher this should be
@@ -45,16 +45,16 @@ edges_canny = edge(grayROI, "canny");
 % (5) Horizontal angles to be identified in the plate identification scenario
 angles = [-90:0.5:-60, 30:0.5:89]; % [-90:1:-45, 45:1:89]
 
-% (6) Run te Hough Lines algorithm for the detection of horizontal lines
-[H, theta, rho] = hough(edges_canny, 'RhoResolution', 1, 'Theta', angles); %'Theta', -90:0.5:89);
+% (6) Run the Hough Lines algorithm for the detection of horizontal lines
+[H, theta, rho] = hough(edges_canny, 'RhoResolution', 1, 'Theta', angles);
 
 % (7a) Identify a set of peaks in the Hough accumulation matrix
-doc_peaks = houghpeaks(H, N_PEAKS);
+met_1_peaks = houghpeaks(H, N_PEAKS);
 
 % (7b) Get rows and thetas of lines occurring above the {HOUGH_THRESHOLD}
 logic_nonzero = H>=HOUGH_THRESHOLD;
 [rows, cols] = find(logic_nonzero);
-my_peaks = [rows, cols];
+met_2_peaks = [rows, cols];
 
 % (8) Find the top P lines previously identified
 % Fillgap: Distance between two line segments associated with the same
@@ -63,80 +63,73 @@ my_peaks = [rows, cols];
 % size(grayROI, 2)
 minLength = size(grayROI, 2)*MIN_LEN_FRACTION;
 fillGap = size(grayROI, 2)*FILL_GAP_FRACTION;
-doc_lines = houghlines(edges_canny, theta, rho, doc_peaks, 'FillGap', fillGap, 'MinLength', minLength);
-my_lines = houghlines(edges_canny, theta, rho, my_peaks, 'FillGap', fillGap, 'MinLength', minLength);
+met_1_lines = houghlines(edges_canny, theta, rho, met_1_peaks, 'FillGap', fillGap, 'MinLength', minLength);
+met_2_lines = houghlines(edges_canny, theta, rho, met_2_peaks, 'FillGap', fillGap, 'MinLength', minLength);
 
 close all;
 
-% (9a) Plot every peak line - Documentation lines
-lines = doc_lines;
-subplot(2,1,1), imshow(grayROI), title('(9a) Detected lines - Doc version'), hold on;
-for i = 1:length(lines)
-    xy = [lines(i).point1; lines(i).point2];
-    plot(xy(:, 1), xy(:, 2), 'LineWidth', 1, 'Color', 'green');
-
-    % Progression output
-    log = sprintf('%d/%d doc lines', i, size(lines, 2));
-    disp(log);
-end
-hold off;
-
-% (9b) Plot every peak line - My lines
-lines = my_lines;
-subplot(2,1,2), imshow(grayROI), title('(9b) Detected lines - My version'), hold on;
-for i = 1:length(lines)
-    xy = [lines(i).point1; lines(i).point2];
-    plot(xy(:, 1), xy(:, 2), 'LineWidth', 1, 'Color', 'green');
-
-    % Progression output
-    log = sprintf('%d/%d my lines', i, size(lines, 2));
-    disp(log);
-end
-hold off;
-
-% (10) Plot only the lines with the most frequent theta on the ROI
-
-% Calculate the most common theta
-lines = my_lines;
-rotations = zeros(length(lines), 1);
-for i = 1:length(lines)
-    rotations(i) = lines(i).theta;
-end
-rotation_factor = mode(rotations);
-
-% Plot lines on the ROI
-figure('Name','(10) Only the lines with the most frequent theta'), imshow(grayROI);
-hold on;
-j = 0;
-for i = 1:length(lines)
-    if lines(i).theta == rotation_factor
+% (9) Plot every peak line - first and second method plot
+lines = met_1_lines;
+for k = 1:2
+    subplot(2,1,k), imshow(grayROI), title('(9) Detected lines - Method %d', k);
+    hold on;
+    for i = 1:length(lines)
         xy = [lines(i).point1; lines(i).point2];
         plot(xy(:, 1), xy(:, 2), 'LineWidth', 1, 'Color', 'green');
     
         % Progression output
-        j = j + 1;
-        log = sprintf('%d final lines', j);
+        log = sprintf('%d/%d lines on plot %d', i, size(lines, 2), k);
+        disp(log);
+    end
+    hold off;
+
+    % Switch to the second method and repeat
+    lines = met_2_lines;
+end
+
+% (10) Plot only the lines with the most frequent theta on the ROI
+
+% Calculate the most common theta through the second method
+lines = met_2_lines;
+rotations = zeros(length(lines), 1);
+for i = 1:length(lines)
+    rotations(i) = lines(i).theta;
+end
+rotation_mode = mode(rotations);
+
+% Plot lines on the ROI
+figure('Name','(10) Only the lines with the most frequent theta'), imshow(grayROI);
+hold on;
+k = 0;
+for i = 1:length(lines)
+    if lines(i).theta == rotation_mode
+        xy = [lines(i).point1; lines(i).point2];
+        plot(xy(:, 1), xy(:, 2), 'LineWidth', 1, 'Color', 'green');
+    
+        % Progression output
+        k = k + 1;
+        log = sprintf('%d final lines', k);
         disp(log);
     end
 end
 hold off;
 
 % (11) Rotate the image
-if rotation_factor > 0
-    rotation = -(90 - rotation_factor);
+if rotation_mode > 0
+    rotation_final = -(90 - rotation_mode);
 else
-    rotation =  (90 + rotation_factor);
+    rotation_final =  (90 + rotation_mode);
 end
 
-if isnan(rotation)
+if isnan(rotation_final)
     error(['ERROR: No horizontal lines where identified! Try decreasing the ' ...
         '{HOUGH_THRESHOLD} or changing the region of interest']);
 else
-    rotatedROI = imrotate(grayROI, rotation, 'bilinear');
+    rotatedROI = imrotate(grayROI, rotation_final, 'bilinear');
     figure, imshow(rotatedROI), title('(11) Rotated ROI according to lines');
 end
 
-disp('all done.');
+disp('Main execution done.');
 
 if DEBUG
     disp(' DEBUGGING NOW');
@@ -147,16 +140,16 @@ if DEBUG
     %     disp(log);
     % end
     
-    peaks = doc_peaks;
-    disp("Printing doc peaks");
-    for j = 1:2
+    peaks = met_1_peaks;
+    disp("Printing the first method's peaks");
+    for k = 1:2
         disp(peaks);
         for i = 1:size(peaks, 1)
             disp(H(peaks(i, 1), peaks(i, 2)));
         end
-        if j == 1
-            disp("Printing my peaks");
-            peaks = my_peaks;
+        if k == 1
+            disp("Printing second method peaks");
+            peaks = met_2_peaks;
         end
     end
 end
